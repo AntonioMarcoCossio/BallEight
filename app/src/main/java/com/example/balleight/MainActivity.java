67203582dff7cd67fa6f4c5ae0c6165f37fc36dc
antonioc <antonio.cossio@innovactors.it>
2023-02-14 17:21:13 +0100

    Initial commit

---
+++ app/src/main/java/com/example/balleight/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.balleight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button pulsante = findViewById(R.id.RollButton);
+        pulsante.setOnClickListener();
+
+
+
+
+    }
+}
